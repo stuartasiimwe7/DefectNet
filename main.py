05e6824971eed7ffc9e6d5836f54114ed51623fc
Stuart Asiimwe <stuartgabriel@ymail.com>
2025-02-07 15:48:10 +0900

    Add Dockerfile and FastAPI application for model prediction service

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, File, UploadFile
+import torch
+from PIL import Image
+import io
+import numpy as np
+
+app = FastAPI()
+
+#Loading model
+model = torch.load("trained_model.pt")
+model.eval() 
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+
+    img = np.array(image) / 255.0  # Normalize
+
+    #prediction
+    results = model(img)
+
+    #result as a JSON
+    return {"predictions": results.pandas().xywh.to_dict(orient="records")}
